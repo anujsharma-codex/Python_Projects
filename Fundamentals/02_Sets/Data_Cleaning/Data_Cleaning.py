630b5d06b106fb7565981cb9a32e0cbf0ac0a04d
anujsharma-codex <anujsharma.codex@gmail.com>
2026-02-13 21:56:00 +0530

    Update Data_Cleaning.py

--- Fundamentals/02_Sets/Data_Cleaning/Data_Cleaning.py
+++ Fundamentals/02_Sets/Data_Cleaning/Data_Cleaning.py
@@ -6,15 +6,17 @@ list_b = ["b@gmail.com", "c@gmail.com", "d@gmail.com"]
 #1
 set1=set(list_a)
 set2=set(list_b)
-print(set1,set2)
+print("unique elements of first list: ",set1)
+print("unique elements of second list: ",set2)
+
 #2
 sa=set(list_a)
 sb=set(list_b)
 s=set1.intersection(set2)
-print(s)
+print("emails in both sets: ",s)
 
 #3
 unique_a=set1.difference(set2)
 unique_b=set2.difference(set1)
-print(unique_a,unique_b)
-
+print("elements only in first: ",unique_a)
+print("elements only in second",unique_b)
